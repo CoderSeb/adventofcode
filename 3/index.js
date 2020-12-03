048466683846e7a8e20a67f4941db19ccdd38278
Sebastian <sa224ny@student.lnu.se>
2020-12-03 13:42:26 +0100

    Added readme and solution for day 3

--- 3/index.js
+++ 3/index.js
@@ -2,23 +2,24 @@ const fs = require('fs')
 
 const theWoods = fs.readFileSync('./3.txt', 'utf8').split(/\r?\n/)
 
-const indexCounter = 3
-
-String.prototype.replaceAt = function(index, replacement) {
-  if (index >= this.length) {
-      return this.valueOf();
+const countTrees = function (slopeX, slopeY) {
+  let x = 0
+  let y = 0
+  let trees = 0
+  while (y < theWoods.length) {
+    trees += theWoods[y][x % theWoods[0].length] === '#'
+    x += slopeX
+    y += slopeY
   }
-  let chars = this.split('');
-  chars[index] = replacement;
-  return chars.join('');
+  return trees
 }
 
-theWoods.forEach(row => {
-  if (row[indexCounter] === '#') {
-    row.replaceAt(indexCounter, 'X')
-    console.log(row)
-  }
-  
-  //indexCounter += 3
-})
+// Part One
+console.log(countTrees(3, 1))
 
+// Part Two
+console.log(countTrees(1, 1)
+          * countTrees(3, 1)
+          * countTrees(5, 1)
+          * countTrees(7, 1)
+          * countTrees(1, 2))
